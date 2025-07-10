878a21ca952b9710e33e3653d63a7d1b68cf129a
tharunteja18 <tharunteja114@gmail.com>
2025-07-10 18:53:48 +0530

    Initial commit.

---
+++ email-generator-service/src/main/java/com/nerchuko/emailgeneratorservice/EmailGeneratorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nerchuko.emailgeneratorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailGeneratorServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailGeneratorServiceApplication.class, args);
+    }
+
+}
---
+++ email-generator-service/src/test/java/com/nerchuko/emailgeneratorservice/EmailGeneratorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nerchuko.emailgeneratorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailGeneratorServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
